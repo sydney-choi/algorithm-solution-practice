c608b66926fa958772b35e75cbafd11aa7457a66
sydney-choi <soyeong0730@gmail.com>
2023-05-25 09:03:14 +0900

    아이스아메리카노

---
+++ programmers/아이스아메리카노.js
@@ -0,0 +1,7 @@
+function solution(money) {
+    return [Math.floor(money / 5500), money % 5500];
+}
+
+// Math.ceil() : 소수점 올림, 정수 반환
+// Math.floor() : 소수점 버림, 정수 반환
+// Math.round() : 소수점 반올림, 정수 반환
