cab29fe67237b27fe60d0b29240596eb681af908
sydney-choi <soyeong0730@gmail.com>
2023-07-11 23:29:17 +0900

    피자 나눠 먹기 (2)

---
+++ programmers/피자나눠먹기(2).js
@@ -0,0 +1,21 @@
+function solution(n) {
+    let lcm =  n * 6 / gcd(n,6);
+    return lcm / 6;
+}
+
+function gcd(a,b){ 
+  return b ? gcd(b, a%b) : a; 
+}
+
+const solution2 = (n) => {
+    let piece = 6
+
+    while(true) {
+        if (piece % n === 0) {
+            break
+        }
+        piece += 6
+    }
+
+    return piece / 6
+}
